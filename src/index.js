6eaea67d790d371dbf666aaeef3c16987ec6c7c1
Tatiana Spiridonova <tn.spiridonova@yandex.ru>
2022-08-05 13:54:24 +0300

    feat: create a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
+module.exports = reverse = (n) => {
+
+  const result = Number(Math.abs(n).toString().split('').reverse().join(''));
+
+  console.log(result)
   
+  return result
 }
+
+// 123 => 321
+
+
+reverse(123566)
